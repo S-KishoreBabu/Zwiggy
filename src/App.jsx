42d38347f63a1dc245eecc278d0ad8356941be26
S-KishoreBabu <s.kishorebabu8@gmail.com>
2026-03-19 10:22:35 +0530

    implemented tanStackQuery

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,20 @@ import {Provider} from "react-redux"
 import reduxStore from './utils/reduxStore'
 import { cartContext } from './utils/UserContext'
 import { useState } from 'react'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 function App() {
   const [cartCount,setCartCount] = useState(0)
-
+  const queryClient = new QueryClient();
   return (
     <cartContext.Provider value={{cartCount,setCartCount}}>
+    <QueryClientProvider client={queryClient} >
     <Provider store={reduxStore}>
       <div className="app bg-[#f5f5f5] ">
         <Header />
         <Outlet />   {/* Body or About will load here */}
       </div>
     </Provider>
+    </QueryClientProvider>
     </cartContext.Provider>
   )
 }
--- src/components/CardWrapper.jsx
+++ src/components/CardWrapper.jsx
@@ -1,29 +1,38 @@
 import { useState, useEffect } from 'react';
 import Card,{PromotedCard} from './Card';
 import CardWrapperShimmer from './CardWrapperShimmer';
-import { Link } from 'react-router-dom';
+import { data, Link } from 'react-router-dom';
 import useOnlineStatus from '../utils/useOnlineStatus';
-
-function CardWrapper({ searchVal }) {
-  const [allProducts, setAllProducts] = useState([]);
-  const [products, setProducts] = useState([]);
-  const onlineStatus = useOnlineStatus();
-  const RestaurantCardPromoted = PromotedCard(Card);
-  // getData();
+import { useQuery } from '@tanstack/react-query';
 
   const getData = async () => {
     let data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.023448&lng=76.9794276&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     let jsonData = await data.json();
-    let restaurants =
-      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    setAllProducts(restaurants || []);
-    setProducts(restaurants || []);
+    return jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
   }
+
+
+function CardWrapper({ searchVal }) {
+  const [allProducts, setAllProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const onlineStatus = useOnlineStatus();
+  const RestaurantCardPromoted = PromotedCard(Card);
+
+
+  const {data:posts, isLoading, error, isFetching} = useQuery({
+    queryKey:['posts'],
+    queryFn:getData
+  });
+
   useEffect(() => {
-    getData();
-  }, []);
+    if (posts) {
+      setAllProducts(posts);
+      setProducts(posts);
+    }
+  }, [posts]);
+
   useEffect(() => {
     if (searchVal) {
       let filtered = allProducts.filter((data) =>
@@ -35,16 +44,13 @@ function CardWrapper({ searchVal }) {
       setProducts(allProducts);
     }
   }, [searchVal, allProducts]);
-  if(!onlineStatus){
-    return (
-      <h2>You are Offline</h2>
-    )
-  }
-  return (!products.length) ? (<CardWrapperShimmer />) : (
+
+  if(!onlineStatus) return (<h2>You are Offline {error.message}</h2>)
+  if(isLoading) return  (<CardWrapperShimmer />)
+  return (
       <div className="cardWrapper flex flex-wrap justify-center gap-7 px-5 py-10 ">
         {products.map((obj) => (
           <Link key={obj.info.id} to={"/restaurant/"+obj.info.id} >
-
             {(Number(obj.info.id) > 500000)?(<RestaurantCardPromoted obj={obj} />):(<Card  obj={obj}/>)}
             
           </Link>
